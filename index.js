778b17e3b4a2f76f0bd868bc2d5fa7189a1eec8a
Mahmoud Mohamed <95969990+MAHMOUD1122q@users.noreply.github.com>
2024-04-06 01:10:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({credentials: true,
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
+  "origin": "https://shop-quest-frontend.vercel.app",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
   }));
 
 const port = process.env.SERVER_PORT || 4000;
